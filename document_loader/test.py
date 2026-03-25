15acd774dc7d091032e8cdfbe4ee150b943c7cc6
MAYANK SAHU <harnarayansahu617@gmail.com>
2026-03-26 02:00:00 +0530

    modified file added

--- document_loader/test.py
+++ document_loader/test.py
@@ -1,5 +1,12 @@
 from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+splitter=CharacterTextSplitter(
+    separator="",
+    chunk_size=10,
+    chunk_overlap=1
+)
 data=TextLoader("document_loader/note.txt")
 result=data.load()
-print(result[0].metadata)
-print(len(result))
+chunks=splitter.split_documents(result)
+print(chunks)
+print(len(chunks))
