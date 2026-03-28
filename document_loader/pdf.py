81b7d0c8fbf2a0e18476a0e34196ec18e24d3cc5
MAYANK SAHU <harnarayansahu617@gmail.com>
2026-03-29 00:30:36 +0530

    modified file added

--- document_loader/pdf.py
+++
@@ -1,5 +0,0 @@
-from langchain_community.document_loaders import PyPDFLoader
-data=PyPDFLoader("document_loader/ms.pdf")
-docs=data.load()
-print(docs)
-print(len(docs))
--- document_loader/webpage.py
+++
@@ -1,5 +0,0 @@
-from langchain_community.document_loaders import WebBaseLoader
-data=WebBaseLoader("https://docs.mistral.ai/")
-docs=data.load()
-print(docs)
-print(len(docs))
